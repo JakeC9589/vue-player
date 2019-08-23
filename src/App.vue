032e031adb715e2f5d3b23268d4e3eaa99ed8472
Jakec9589 <jakec9589@gmail.com>
2019-08-23 15:40:38 -0400

    Base of a larger project
    A base I think would work decently for a larger project about converting playlists from YouTube to Spotify and vice versa.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+
+    <!--
+      References:
+      https://youtu.be/ZSexhaiMKJE
+      https://youtu.be/78tNYZUS-ps
+      DesignCourse on youtube: https://www.youtube.com/channel/UCVyRiMvfUNMA1UPlDPzG5Ow
+    -->
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+</style>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Stats.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <h1>A different Component</h1>
+    <p>There are currently {{ countLinks }} links</p>
+
+    <button v-on:click="removeAllLinks">Remove all links</button>
+    <p>{{ msg }}</p>
+  </v-container>
+</template>
+
+<script>
+  import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'Stats',
+  data(){
+    return{
+      msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ]),
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks() {
+      this.removeAll().then(() =>{
+        this.msg = 'They have been removed'
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
